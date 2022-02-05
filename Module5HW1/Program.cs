8a6fd170eb41a27e8f0a02f9c59233a3afee4bcd
KarenVardanian <karen.vardanian@nure.ua>
2022-02-05 13:52:40 +0200

    Добавьте файлы проекта.

---
+++ Module5HW1/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace Module5HW1
+{
+    public class Program
+    {
+        public void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+
+        }
+    }
+}
---
+++ Module5HW1/Services/HttpService.cs
@@ -0,0 +1,7 @@
+﻿namespace Module5HW1.Services
+{
+    public class HttpService
+    {
+
+    }
+}
